d46074e69f7117f785768cccc39e134d104bc9b3
Shivam Rastogi <shivam@Shivams-MacBook-Air.local>
2016-08-03 18:31:21 +0530

    The code given by Udacity for this project. (https://github.com/udacity/StockHawk)

---
+++ app/src/main/java/com/example/shivam/stockr/touch_helper/ItemTouchHelperViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.shivam.stockr.touch_helper;
+
+/**
+ * Created by sam_chordas on 10/6/15.
+ * credit to Paul Burke (ipaulpro)
+ * Interface for enabling swiping to delete
+ */
+public interface ItemTouchHelperViewHolder {
+  void onItemSelected();
+
+  void onItemClear();
+}
